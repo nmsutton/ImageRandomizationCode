a37269b9afdde1dbad8c006fbda5d9a51b1eaee4
Nate Sutton <natemsutton@gmail.com>
2014-11-24 02:07:45 -0500

    Changed commenting

--- Approach1.m
+++ Approach1.m
@@ -1,9 +1,9 @@
 function Approach1()
 indices = [9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9]
-leftTargets1 = floor(rand(1)*9)+1; #Random number between 1-9
+leftTargets1 = floor(rand(1)*9)+1; %Random number between 1-9
 leftTargets2 = 9 - leftTargets1
-targetCriteria1 = randperm(9); #Numbers 1-9 in random order
-targetCriteria2 = randperm(9); #Numbers 1-9 in random order
+targetCriteria1 = randperm(9); %Numbers 1-9 in random order
+targetCriteria2 = randperm(9); %Numbers 1-9 in random order
 bottomImageSameAsTop = []
 for i = 1:18
   if i < 9
@@ -15,9 +15,9 @@ for i = 1:18
   end
   
   if target_criteria(indices(i)) <= left_bottom_image_cutoff
-    bottomImageSameAsTop = [bottomImageSameAsTop, ' left '] #left bottom is same as top image
+    bottomImageSameAsTop = [bottomImageSameAsTop, ' left '] %left bottom is same as top image
   else
-    bottomImageSameAsTop = [bottomImageSameAsTop, ' right '] #right bottom is same as top image
+    bottomImageSameAsTop = [bottomImageSameAsTop, ' right '] %right bottom is same as top image
   end
 end
 
